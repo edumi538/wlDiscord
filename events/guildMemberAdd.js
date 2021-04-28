f4642c483f5b2489e6612765aedc37e09344e919
ojota-dev <72539739+ojota-dev@users.noreply.github.com>
2021-04-28 05:13:59 -0300

    updating

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,11 @@
+const moment = require('moment')
+module.exports.run = async (member, client) => {
+    const newUser = ({
+        status: 'newUser',
+        user: client.user.username,
+        userId: client.user.id,
+        discriminator: client.user.discriminator,
+        daysSinceCreation: moment().diff(moment(client.user.createdAt), 'days')
+    })
+    if (newUser.daysSinceCreation < 3) return (() => { console.log({ antiSelf: true, userId: newUser.userId, return: 'Possivel Selfbot'}) })()
+}
